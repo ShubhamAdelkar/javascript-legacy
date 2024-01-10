33b74ee6ebfc12e361cb29a108626f6f8e830ebd
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-10 14:30:08 +0530

    problem 5

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+function reverse(string) {
+  let reversed = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i];
+  }
+  return reversed;
+}
+
+module.exports = reverse;
+console.log(reverse("cat"));
