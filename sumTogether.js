59b15faeb252798e6db9d0f8a900c7ee960032a5
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-10 14:50:35 +0530

    problem 7

---
+++ sumTogether.js
@@ -0,0 +1,10 @@
+function sumTogether(arr1, arr2) {
+  let sum = [];
+  for (let i = 0; i < arr1.length; i++) {
+    sum.push(arr1[i] + arr2[i]);
+  }
+  return sum;
+}
+
+module.exports = sumTogether;
+console.log(sumTogether([2, 3, 4, 5], [3, 5, 7, 54]));
