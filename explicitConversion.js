3afa25f7feac5d2362bdc50103b2c52b0f130cd5
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-14 23:11:54 +0530

    type coersion

---
+++ explicitConversion.js
@@ -0,0 +1,4 @@
+// explicit conversion
+const a = (1).toString();
+console.log(a); // 1
+console.log(typeof a); // string
---
+++ implicitConversion.js
@@ -0,0 +1,4 @@
+// implicit conversion
+const b = "3" + 4;
+console.log(b); // 34
+console.log(typeof b); // String
