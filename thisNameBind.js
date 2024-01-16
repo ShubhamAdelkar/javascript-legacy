f3a2c5e33d4e56159f95359839cb790eb2c5c2d5
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-16 22:26:31 +0530

    Bind instead call

---
+++ thisNameBind.js
@@ -0,0 +1,12 @@
+//same but with bind() instead call().
+
+function thisName() {
+  return this.name;
+}
+
+const newFunction = thisName.bind({ name: "Shubham" });
+const newFunction2 = thisName.bind({ name: "Bob" });
+console.log(newFunction()); // Shubham
+console.log(newFunction2()); //Bob
+
+module.exports = newFunction2;
