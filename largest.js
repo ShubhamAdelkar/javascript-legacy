c463a1c70ac08e792820215bad4b2f5d6069f4f0
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-22 19:34:18 +0530

    finding largest value using reduce.

---
+++ largest.js
@@ -0,0 +1,10 @@
+// finding largest value using reduce.
+function largest(numbers) {
+  return numbers.reduce((accumulator, currentValue) => {
+    return accumulator > currentValue ? accumulator : currentValue;
+  });
+}
+
+console.log(largest([1, 2, 3, 4, 5, 6, 7, 8]));
+
+module.exports = largest;
