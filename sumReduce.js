727910e63ae20cc0a9b20120209460eade49f7bf
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-22 13:16:27 +0530

    array reduce example

---
+++ sumReduce.js
@@ -0,0 +1,8 @@
+function sum(numbers) {
+  return numbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue; 
+  });
+}
+
+console.log(sum([1, 2, 3, 4, 5]));
+module.exports = sum;
