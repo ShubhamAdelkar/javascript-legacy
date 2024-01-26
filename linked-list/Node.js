f4e50671cb154e38879ad8e5ea4f6070360535b5
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-01-26 21:24:36 +0530

    linked list first node without next.

---
+++ linked-list/Node.js
@@ -0,0 +1,12 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+const node = new Node(4);
+console.log(node.data);
+console.log(node.next);
+
+module.exports = Node;
