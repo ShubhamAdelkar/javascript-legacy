1387365d48d01ab67aa7b127e6f7539a7733bace
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-07 21:30:30 +0530

    rest of the array into seperate array

--- ES6/arrayDestructing.js
+++ ES6/arrayDestructing.js
@@ -10,3 +10,13 @@ console.log(([a, b] = [b, a]));
 // destructing using commas to reach the desired index
 const [p, q, , , r] = [1, 2, 3, 4, 5, 6];
 console.log(p, q, r);
+
+// collecting rest of the array into seperate array
+function removeFirstTwo(list) {
+  const [a, b, ...arr] = list;
+  console.log("removed elements " + a, b);
+  return arr;
+}
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+console.table(sourceWithoutFirstTwo);
