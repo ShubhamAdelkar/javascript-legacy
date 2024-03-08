e592a8f34b246ab31fe464c12e58f14a7380b06f
Shubham Adelkar <adelkarshubham47@gmail.com>
2024-03-08 21:46:03 +0530

    template literals

---
+++ ES6/templateLiterals.js
@@ -0,0 +1,25 @@
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56,
+};
+
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"],
+};
+function makeList(arr) {
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+console.log(failuresList);
