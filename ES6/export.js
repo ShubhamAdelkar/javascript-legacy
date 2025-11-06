4669443d7c8e3b34cf1275efb4e81ecfc47bb20a
Shubham Adelkar <adelkarshubham47@gmail.com>
2025-11-06 12:55:32 +0530

    es6 feature

---
+++ ES6/export.js
@@ -0,0 +1,13 @@
+// Export a single variable (named export)
+export const MAX_SCORE = 100;
+
+// Export a function
+export function calculateAverage(scores) {
+  // ... calculation logic
+  return average;
+}
+
+// Export a default item (only one allowed per file)
+export default class UserProfile {
+  /* ... */
+}
---
+++ ES6/import.js
@@ -0,0 +1,8 @@
+// Import the default export (you can name it anything)
+import UserProfile from "./export.js";
+
+// Import the named exports
+import { MAX_SCORE, calculateAverage } from "./export.js";
+
+console.log(MAX_SCORE);
+// Output: 100
